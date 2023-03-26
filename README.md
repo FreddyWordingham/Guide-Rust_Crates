ed5671394eb5fb9338a1f10730bd2b7e0612c2f9
FreddyWordingham <freddy@digilab.co.uk>
2023-03-26 17:28:04 +0100

    Added images chapter

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ How to build Rust crates using Cargo.
 -   [2 Library](./chapters/2-Library/README.md)
 -   [3 Test](./chapters/3-Test/README.md)
 -   [4 Plotting](./chapters/4-Plotting/README.md)
+-   [5 Images](./chapters/5-Images/README.md)
---
+++ chapters/5-Images/src/bin/main.rs
@@ -0,0 +1,15 @@
+use mandy::{colour, sample};
+
+/// Entry point function.
+fn main() {
+    let real = 0.42883258532;
+    let imag = -0.23134912185;
+    let scale = 1.0e-1;
+    let max_iters = 1000;
+    let res = [1920, 1080];
+    let cmap = vec!["#000000", "#FFFFFF"];
+
+    let data = sample::area(real, imag, scale, res, max_iters);
+    let mut img = colour::image(data, cmap, max_iters);
+    colour::encode(&mut img).save("mandy.png").unwrap();
+}
